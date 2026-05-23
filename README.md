22d925e5bc6ff065f6c77f91a61318f49eb6da0a
schweinsteiger-f <schweinsteiger.pacheco@gmail.com>
2026-05-23 19:11:27 -0300

    Update 01

---
+++ README.md
@@ -0,0 +1,20 @@
+# Calculadora **_Python_** 🐍
+
+## E simplesmente um google maps feito em pyton nada de especial🫠
+
+<details>
+<summary>✨Bibliotecas utilizadas✨</summary>
+1. Colorama
+<br>
+2. Webbrowser
+<br>
+3. Urllib
+<br>
+4. Time
+</details>
+
+### Aproveite o projeto!
+
+### [Clique aqui para aprender a executar o arquivo!](https://github.com/schweinsteiger-f/PY-Global)
+
+Ass: **_Schweinsteiger Pacheco Dias :3_**
---
+++ main.py
@@ -0,0 +1,22 @@
+# Python Google-Maps V.0.0.1
+import webbrowser
+import urllib
+from urllib import parse
+import colorama
+from colorama import Fore, Back, Style
+import time
+colorama.init()
+print(Fore.LIGHTCYAN_EX + "Bem-vindo ao Gmaps em python!")
+print("Iniciando....")
+time.sleep(2)
+localidade_codificada = urllib.parse.quote_plus(input("Digite aqui a localidade que você gostaria de pesquisar: "))
+link = f"https://www.google.com/maps/search/?api=1&query={localidade_codificada}"
+print(f"aqui esta o link: {link}")
+quest = input("Quer ir ate o site? Sim(1) Não(2)")
+if quest == "1":
+    print(Fore.RED + "Adeus!")
+    time.sleep(1)
+    webbrowser.open(link)
+else:
+    None
+    print(Fore.RED + "Adeus!")
